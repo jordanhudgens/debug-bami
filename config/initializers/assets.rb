4c61542346dc77208d1ff2adfd07912ddffcce51
bamidips <veerster@gmail.com>
2014-11-09 00:52:52 +0000

    added some bootstrap styling

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile +=[/.*\.js/,/.*\.css/] 
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-white.png )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings.png )
--- db/migrate/20141108192023_create_invoices.rb
+++ db/migrate/20141108192023_create_invoices.rb
@@ -2,6 +2,7 @@ class CreateInvoices < ActiveRecord::Migration
   def change
     create_table :invoices do |t|
       t.datetime :date
+      
       t.string :company
       t.decimal :tax
       t.string :salesperson
